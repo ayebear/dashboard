33c61372fd21c72a46a472041f7cb5ea2c416a89
Eric Hebert <ayebear@gmail.com>
2023-07-17 00:27:47 -0400

    Working clock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::prelude::*;
 use notan::prelude::*;
 use notan::text::*;
 
@@ -33,22 +34,26 @@ fn setup(gfx: &mut Graphics) -> State {
 }
 
 fn draw(gfx: &mut Graphics, state: &mut State) {
+    let date_as_string = Utc::now().to_string();
+    let (width, height) = gfx.size();
+    let (cw, ch) = ((width as f32) / 2.0, (height as f32) / 2.0);
+
     let mut text = gfx.create_text();
     text.clear_color(Color::BLACK);
 
-    text.add("Hello ")
+    text.add(&date_as_string)
         .font(&state.font)
-        .position(400.0, 30.0)
+        .position(cw, ch - 64.0)
         .h_align_center()
         .color(Color::ORANGE)
-        .size(30.0);
+        .size(128.0);
 
-    text.chain("Notan! ").size(50.0).color(Color::RED);
+    // text.chain("Notan! ").size(50.0).color(Color::RED);
 
-    text.chain("(Using TextExtension)")
-        .font(&state.font2)
-        .size(20.0)
-        .color(Color::GRAY.with_alpha(0.5));
+    // text.chain("(Using TextExtension)")
+    //     .font(&state.font2)
+    //     .size(20.0)
+    //     .color(Color::GRAY.with_alpha(0.5));
 
     gfx.render(&text);
 }
