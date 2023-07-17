cd525b2dcc146f115670ba334e3196554dbb838a
Eric Hebert <ayebear@gmail.com>
2023-07-17 02:03:22 -0400

    Add placeholders for weather/stocks

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use notan::prelude::*;
 use notan::text::*;
 
 const FONT_SIZE: f32 = 128.0;
+const PADDING: f32 = 32.0;
 
 #[derive(AppState)]
 struct State {
@@ -32,17 +33,28 @@ fn setup(gfx: &mut Graphics) -> State {
 
 fn draw(gfx: &mut Graphics, state: &mut State) {
     let date_as_string = Local::now().format("%A %B %d,  %I:%M:%S %p").to_string();
-    let (width, height) = gfx.size();
-    let (cx, cy) = ((width as f32) / 2.0, (height as f32) / 2.0);
+    let (width, _) = gfx.size();
+    let cx = (width as f32) / 2.0;
 
     let mut text = gfx.create_text();
     text.clear_color(Color::BLACK);
 
     text.add(&date_as_string)
         .font(&state.font)
-        .position(cx, cy - FONT_SIZE / 2.0)
-        .h_align_center()
+        .position(PADDING, PADDING)
         .color(Color::ORANGE)
+        .size(FONT_SIZE);
+
+    text.add("72-90 °F\n82%")
+        .font(&state.font)
+        .position(PADDING, PADDING * 2.0 + FONT_SIZE)
+        .color(Color::AQUA)
+        .size(FONT_SIZE);
+
+    text.add("NVDA $542.69\nAMD $157.24\nTSLA $303.89")
+        .font(&state.font)
+        .position(cx, PADDING * 2.0 + FONT_SIZE)
+        .color(Color::GREEN)
         .size(FONT_SIZE);
 
     gfx.render(&text);
