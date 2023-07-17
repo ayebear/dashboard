7812b30f2361a0b0103221bf4c5b67cc6355a81f
Eric Hebert <ayebear@gmail.com>
2023-07-17 00:20:10 -0400

    Setup notan with font example and fullscreen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use notan::prelude::*;
+use notan::text::*;
+
+#[derive(AppState)]
+struct State {
+    font: Font,
+    font2: Font,
+}
+
+#[notan_main]
+fn main() -> Result<(), String> {
+    let win_config = WindowConfig::new()
+        .resizable(true)
+        .maximized(true)
+        .fullscreen(true);
+    notan::init_with(setup)
+        .add_config(win_config)
+        .add_config(TextConfig)
+        .draw(draw)
+        .build()
+}
+
+fn setup(gfx: &mut Graphics) -> State {
+    let font = gfx
+        .create_font(include_bytes!("../assets/Ubuntu-B.ttf"))
+        .unwrap();
+
+    let font2 = gfx
+        .create_font(include_bytes!("../assets/kenney_pixel-webfont.ttf"))
+        .unwrap();
+
+    State { font, font2 }
+}
+
+fn draw(gfx: &mut Graphics, state: &mut State) {
+    let mut text = gfx.create_text();
+    text.clear_color(Color::BLACK);
+
+    text.add("Hello ")
+        .font(&state.font)
+        .position(400.0, 30.0)
+        .h_align_center()
+        .color(Color::ORANGE)
+        .size(30.0);
+
+    text.chain("Notan! ").size(50.0).color(Color::RED);
+
+    text.chain("(Using TextExtension)")
+        .font(&state.font2)
+        .size(20.0)
+        .color(Color::GRAY.with_alpha(0.5));
+
+    gfx.render(&text);
 }
